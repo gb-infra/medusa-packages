d36737cdad72d124bd1acbb73affccb15915045e
Shivam Pandey <shivam.pandey@globalbees.com>
2024-12-13 12:52:23 +0530

    chore: minor change remove not used packages

--- packages/strapi-sync/src/index.ts
+++ packages/strapi-sync/src/index.ts
@@ -1,6 +1,11 @@
 import { Module } from "@medusajs/framework/utils";
 import { UpdateStrapiService } from "@services";
 
+export * from "@utils";
+export * from "@services";
+export * from "@types";
+export * from "@constants";
+
 export const CMS_MODULE = "cms";
 export default Module(CMS_MODULE, {
   service: UpdateStrapiService,
